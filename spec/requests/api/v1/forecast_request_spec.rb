f19e1ae67120e9d78765bb8155e9765a7d6e055c
Chi Tran <chidtran@gmail.com>
2019-06-02 14:31:22 -0600

    Adds additional test

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Forecast do
-  it 'can return forecast weather for location' do 
+  it 'can return weather forecast for city' do 
     get "/api/v1/forecast?location=denver,co"
 
     expected = JSON.parse(response.body)
@@ -9,5 +9,18 @@ describe Forecast do
     expect(response).to be_successful
     expect(expected['data']['id']).to eq('1')
     expect(expected['data']['type']).to eq('forecast')
+    expect(expected['data']['attributes']['forecast_weather']['timezone']).to eq('America/Denver')
   end
+
+  it 'can return weather forecast for another city' do
+    get "/api/v1/forecast?location=chicago,il"
+  
+    expected = JSON.parse(response.body)
+
+    expect(response).to be_successful
+  
+    expect(expected['data']['id']).to eq('1')
+    expect(expected['data']['type']).to eq('forecast')
+    expect(expected['data']['attributes']['forecast_weather']['timezone']).to eq('America/Chicago')
+  end 
 end

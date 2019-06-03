fac9814c80f0765294ca1cef1a9e2332f516d102
Chi Tran <chidtran@gmail.com>
2019-06-02 22:02:02 -0600

    Adds serializers

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,5 +1,18 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :forecast_weather, :id
+  set_id :location_id
+  attributes :city, :state, :latitude, :longitude, :date
+  
+  attribute :current_weather do |object|
+    object.get_current_weather
+  end
+  
+  attribute :weather_hours do |object|
+    object.get_weather_hours
+  end
+  
+  attribute :weather_days do |object|
+    object.get_weather_days
+  end
 end

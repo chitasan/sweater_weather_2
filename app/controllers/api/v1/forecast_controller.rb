0d38ff86667b1daf87dc96e9ecf60c476e3c31d4
Chi Tran <chidtran@gmail.com>
2019-06-05 09:07:52 -0600

    Removes case

---
+++ app/controllers/api/v1/forecast_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::ForecastController < ApplicationController
+  def show
+    forecast = Forecast.new(params[:location])
+    render json: ForecastsSerializer.new(forecast)
+  end 
+end 

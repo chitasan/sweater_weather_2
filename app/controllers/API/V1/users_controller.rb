106f92c9df960f37cb598e1e0c2db9837260f8b9
Chi Tran <chidtran@gmail.com>
2019-06-04 20:21:16 -0600

    Deletes password_confirmation

--- app/controllers/API/V1/users_controller.rb
+++ app/controllers/API/V1/users_controller.rb
@@ -15,4 +15,4 @@ class Api::V1::UsersController < ApplicationController
   def user_params
     params.permit(:email, :password, :password_confirmation)
   end
- end
+end
--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -7,12 +7,11 @@ require 'rails_helper'
     post '/api/v1/sessions', params: {
                                       email: user.email,
                                       password: user.password,
-                                      password_confirmation: user.password_confirmation
                                     }
 
     expect(response.status).to eq(200)
     expected = JSON.parse(response.body, symbolize_names: true)
-                         
+    
     expect(expected).to have_key(:api_key)
     expect(expected[:api_key]).to eq(user.api_key)
   end
